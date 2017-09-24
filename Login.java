5c8665c28edc6918e113607760b36d62c2aba037
Jay Shah <j24shah@gmail.com>
2017-09-23 20:04:35 -0400

    Updated the background, and added more methods.
    I opened up another javaproject and created a package called gui. In there I have java classes. 
    login.java is one of them.

--- Login.java
+++ Login.java
@@ -21,6 +21,7 @@ import javafx.scene.layout.*;
 public class Login extends BorderPane {
 	
 	public Login(){
+		//Initialized objects. 
 		TextField username = new TextField();
 		TextField password = new TextField();
 		Button Login = new Button("Login");
@@ -28,6 +29,9 @@ public class Login extends BorderPane {
 		Label error_label = new Label();
 		error_label.setVisible(false);
 		
+		//Gride pane is located in the center of the border pane. 
+		//holds the text field for user name, and password
+		//holds a button to login. 
 		GridPane grid = new GridPane();
 		grid.setPadding(new Insets(100,20,10,110));//top,right,bottom,left
 		grid.setVgap(5);
@@ -38,27 +42,32 @@ public class Login extends BorderPane {
 		grid.add(Login, 0, 2);
 		
 		
-		
+		//vbox that is set on top of the users and password. 
+		//displays instructions. 
 		VBox vbox = new VBox();
 		vbox.setPadding(new Insets(50,5,5,5)); //top,right,bottom,left
 		vbox.getChildren().add(title);
 		vbox.setAlignment(Pos.CENTER);
 		
+		//vabox pane that is attached to the bottom of the page.
+		//shows the error message if the inputs are wrong. 
 		VBox box = new VBox();
 		box.setPadding(new Insets(5,5,5,5));
 		box.getChildren().add(error_label);
 		box.setAlignment(Pos.TOP_CENTER);
 		
+		//sets the style for this page. 
 		this.setStyle("-fx-background-color:#8FBC8F;");
 		this.setTop(vbox);
 		this.setCenter(grid);
 		this.setBottom(box);
 		
-		
+		//login button's options on click. 
 		Login.setOnAction(
 				(event)->{
 					if ((username.getText().equals("host")) && (password.getText().equals("host"))) {
-						open_new_scene("Host.fxml", Login, "Host Page");
+						//opens up a new page which displays what hosts see. 
+						open_host_scene(Login, "Host Page");
 
 					} else if ((username.getText().equals("manager")) && (password.getText().equals("manager"))) {
 						// open_new_scene("Manager.fxml", login_button, "Manager Page");
@@ -75,30 +84,52 @@ public class Login extends BorderPane {
 	}
 	
 	
-	
-	public void open_new_scene(String fxml_filename, Button button, String title){
+/////////////////////////////////////////////////////////////////////
+	public void open_host_scene(Button button, String title){
 		/*
-		 * This method takes in fxml filename in string format, button name, and
-		 * the title of the new scene in string format.
+		 * This method takes in button name and title for the page. 
+		 * This is for host page. 
 		 * 
 		 */
-
 		// starts a new scene from button click and sets the title.
+		Host_page hp = new Host_page();
 		Stage stage = (Stage) button.getScene().getWindow();
 		stage.setTitle(title);
-		Parent root = null;
-		try {
-			// loads another fxml page.
-			root = FXMLLoader.load(getClass().getResource("/application/" + fxml_filename));
-
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// show the new stage.
+		stage.setScene(new Scene(hp,900,500));
 		stage.show();
 	}
+	
+/////////////////////////////////////////////////////////////////////
+
+	public void open_manager_scene(Button button, String title){
+		/*
+		 * This method takes in button name and title for the page. 
+		 * This is for manager page. 
+		 * 
+		 */
+		// starts a new scene from button click and sets the title.
+		
+		Stage stage = (Stage) button.getScene().getWindow();
+		stage.setTitle(title);
+		//stage.setScene(new Scene(hp,900,500));
+		stage.show();
+	}
+/////////////////////////////////////////////////////////////////////
+	
+	public void open_server_scene(Button button, String title){
+		/*
+		 * This method takes in button name and title for the page. 
+		 * This is for server page. 
+		 * 
+		 */
+		// starts a new scene from button click and sets the title.
+		
+		Stage stage = (Stage) button.getScene().getWindow();
+		stage.setTitle(title);
+		//stage.setScene(new Scene(hp,900,500));
+		stage.show();
+	}
+	
+	
 
 }
