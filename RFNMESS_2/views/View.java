8e77aa0de02ebcbb7106faaa31b7dbf055caf55a
Jay Shah <j24shah@gmail.com>
2017-10-28 19:06:15 -0400

    View calss in a view package.

---
+++ RFNMESS_2/views/View.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package views;
+
+import javafx.scene.Node;
+/**
+ * @author jay_s
+ *
+ */
+public interface View {
+	Node getTop();
+	Node getRight();
+	Node getLeft();
+	Node getBottom();
+	
+}
