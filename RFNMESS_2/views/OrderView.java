468a9f60623d1e969a017e441204b3a41f669def
tajaniz95 <zaheertajani95@gmail.com>
2017-10-31 12:59:20 -0400

    added OrderView

---
+++ RFNMESS_2/views/OrderView.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package views;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.Node;
+
+/**
+ * @author Zaheer Tajani
+ *
+ */
+public class OrderView implements View {
+
+	/* (non-Javadoc)
+	 * @see views.View#getCenter()
+	 */
+	@Override
+	public Node getCenter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see views.View#getRight()
+	 */
+	@Override
+	public Node getRight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see views.View#getLeft()
+	 */
+	@Override
+	public Node getLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see views.View#getBottom()
+	 */
+	@Override
+	public Node getBottom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see views.View#addEventHandler(javafx.event.EventType, javafx.event.EventHandler)
+	 */
+	@Override
+	public <T extends Event> void addEventHandler(EventType<T> eventType, EventHandler<? super T> eventHandler) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
