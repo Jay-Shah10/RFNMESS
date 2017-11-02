5b090b6fb1628a14132dc19a2e3eb20321ac7549
Jay Shah <j24shah@gmail.com>
2017-11-02 12:32:05 -0400

    stackpane

--- RFNMESS_2/gui/NewCenter.java
+++ RFNMESS_2/gui/NewCenter.java
@@ -9,10 +9,17 @@ public class NewCenter extends StackPane {
 	public TableImageButtons tib; 
 	
 	public NewCenter() {
+		/*
+		 * This class is the new center view of the master pane. 
+		 * It is a stack pane, which holds bunch of tables. 
+		 * 
+		 */
 		
 		this.setStyle("-fx-background-color: #ffff");
 		tib = new TableImageButtons(150, 100);
 		
+		// set translatex changes the x values in the direction you want to move the table. 
+		// set translate y changes the y values in the direction you want to move the table. 
 		
 		tib.setTranslateX(60);
 		this.getChildren().add(tib);
