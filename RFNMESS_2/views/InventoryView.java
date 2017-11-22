3897be355c5c4c75b5510a1e3288f73fa7f438ab
tajaniz95 <zaheertajani95@gmail.com>
2017-11-21 20:39:19 -0500

    InventoryView

---
+++ RFNMESS_2/views/InventoryView.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package views;
+
+import javafx.scene.Node;
+
+/**
+ * @author Zaheer Tajani
+ *
+ */
+public class InventoryView implements View {
+	
+	private VBox 
+
+	/* (non-Javadoc)
+	 * @see views.View#getCenter()
+	 */
+	@Override
+	public Node getCenter() {
+		// TODO Auto-generated method stub
+		return center;
+	}
+
+	/* (non-Javadoc)
+	 * @see views.View#getRight()
+	 */
+	@Override
+	public Node getRight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see views.View#getLeft()
+	 */
+	@Override
+	public Node getLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see views.View#getBottom()
+	 */
+	@Override
+	public Node getBottom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public InventoryView() {
+		
+	}
+
+}
